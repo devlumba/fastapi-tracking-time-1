e1c89841e06ff4c6bc48caf4a41875b8a4e6a7e0
unknown <ilovesylphy@gmail.com>
2026-04-15 00:06:23 +0300

    additional routes for precision, update route, stats route, route for all seshs split by category, also dark_swag, more enum types

--- hours.py
+++ hours.py
@@ -2,19 +2,25 @@ from typing import Annotated
 from datetime import date, timedelta
 
 from fastapi import FastAPI, Depends, Path
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import RedirectResponse
 from pydantic import BaseModel
-from sqlmodel import Session, SQLModel, create_engine, Field, select
+from sqlmodel import Session, SQLModel, create_engine, Field, select, func
 from enum import Enum
 from starlette.exceptions import HTTPException
+
+from dark_swag import FastAPI
 
 
 class SeshType(Enum):
     duolingo = "duolingo"
     programming = "programming"
+    words = "words"
+    reading = "reading"
+    juggling = "juggling"
 
 
 class SeshBase(SQLModel):
-    type: SeshType | None = Field(index=True, default=None)
     length: int | None = Field(default=None, index=True)
     specifics: str = Field()
     day: date = Field(index=True)
@@ -22,11 +28,18 @@ class SeshBase(SQLModel):
 
 class Sesh(SeshBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
+    type: SeshType | None = Field(index=True, default=None)
 
 
 class SeshCreate(SeshBase):
     pass
 
+
+class SeshUpdate(SeshBase):
+    length: int | None = None
+    specifics: str | None = None
+    day: date | None = None
+    type: SeshType | None = None
 
 
 sqlite_file_name = "database.db"
@@ -35,6 +48,7 @@ sqlite_url = f"sqlite:///{sqlite_file_name}"
 connect_args = {"check_same_thread": False}
 engine = create_engine(sqlite_url, connect_args=connect_args)
 
+templates = Jinja2Templates(directory="templates-hours")
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
@@ -60,13 +74,18 @@ def on_startup():
     create_db_and_tables()
 
 
-@app.get("/seshs/")
+@app.get("/")
+def root():
+    return RedirectResponse(url="/docs_light")
+
+
+@app.get("/seshs/", tags=["crud"])
 def read_seshs(session: SessionDep):
     seshs = session.exec(select(Sesh)).all()
     return seshs
 
 
-@app.post("/seshs/")
+@app.post("/seshs/", tags=["crud"])
 def create_seshs(sesh: SeshCreate, sesh_type:SeshType, session: SessionDep):
     db_sesh = Sesh.model_validate(sesh)
     db_sesh.type = sesh_type
@@ -76,7 +95,7 @@ def create_seshs(sesh: SeshCreate, sesh_type:SeshType, session: SessionDep):
     return db_sesh
 
 
-@app.delete("/seshs/")
+@app.delete("/seshs/", tags=["crud"])
 def delete_seshs(session: SessionDep, sesh_id: int):
     sesh = session.get(Sesh, sesh_id)
     if not sesh:
@@ -87,16 +106,29 @@ def delete_seshs(session: SessionDep, sesh_id: int):
     return {"msg": "fuck off aye"}
 
 
+@app.put("/seshs/", tags=["crud"])
+def update_sesh(session: SessionDep, sesh: SeshUpdate, sesh_id: int):
+    db_sesh = session.get(Sesh, sesh_id)
+    if not db_sesh:
+        raise HTTPException(status_code=404, detail="nope mate")
+    sesh_data = sesh.model_dump(exclude_unset=True)
+    db_sesh.sqlmodel_update(sesh_data)
+    session.add(db_sesh)
+    session.commit()
+    session.refresh(db_sesh)
+    return db_sesh
+
+
 # basic routes done
 
 
-@app.get("/seshs/programming/") # could be read_type but it's just 2
+@app.get("/seshs/programming/", tags=["programming"]) # could be read_type but it's just 2
 def read_seshs_programming(session: SessionDep):
     seshs = session.exec(select(Sesh).where(Sesh.type == "programming")).all()
     return seshs
 
 
-@app.get("/seshs/duolingo/") # could be read_type but it's just 2
+@app.get("/seshs/duolingo/", tags=["seshs_general"]) # could be read_type but it's just 2
 def read_seshs_duolingo(session: SessionDep):
     seshs = session.exec(select(Sesh).where(Sesh.type == "duolingo")).all()
     return seshs
@@ -113,14 +145,14 @@ def read_seshs_duolingo(session: SessionDep):
 #     return seshs
 
 
-@app.get("/seshs/{age}")
+@app.get("/seshs/time/{age}", tags=["seshs_general"])
 def read_seshs_age(age: int, session: SessionDep):
     cutoff_day = date.today() - timedelta(days=age)
     seshs = session.exec(select(Sesh).where(Sesh.day >= cutoff_day)).all()
     return seshs
 
 
-@app.get("/seshs/{age}/time")
+@app.get("/seshs/time/{age}/time", tags=["seshs_general"])
 def read_time_age(sesh_type: SeshType, age: int, session: SessionDep):
     cutoff_day = date.today() - timedelta(days=age)
     seshs = session.exec(select(Sesh).where(Sesh.type == sesh_type, Sesh.day >= cutoff_day)).all()
@@ -128,4 +160,77 @@ def read_time_age(sesh_type: SeshType, age: int, session: SessionDep):
     for i in seshs:
         sum += i.length
     return {"minutes": sum, "hours": sum/60}
+
+
+@app.get("/seshs/programming/week", tags=["programming"])
+def read_seshs_programming_week(session: SessionDep):
+    cutoff_date = date.today() - timedelta(days=6)
+    time = session.exec(select(func.sum(Sesh.length)).where(Sesh.type == SeshType.programming, Sesh.day >= cutoff_date)).one() or 0
+    return {"minutes last week": time, "hours last week": time/60}
+
+
+@app.get("/seshs/programming/fortnight", tags=["programming"])
+def read_seshs_programming_fortnight(session: SessionDep):
+    cutoff_date = date.today() - timedelta(days=13)
+    time = session.exec(select(func.sum(Sesh.length)).where(Sesh.type == "programming", Sesh.day >= cutoff_date)).one() or 0
+    return {"minutes last fortnight": time, "hours last fortnight": time/60}
+
+
+@app.get("/seshs/programming/month", tags=["programming"])
+def read_seshs_programming_month(session: SessionDep):
+    cutoff_date = date.today() - timedelta(days=29)
+    time = session.exec(select(func.sum(Sesh.length)).where(Sesh.type == "programming", Sesh.day >= cutoff_date)).one() or 0
+    return {"minutes last month": time, "hours last month": time/60}
+
+
+@app.get("/seshs/categorized", tags=["seshs_general"])
+def read_seshs_categorized(session: SessionDep):
+    types = list(SeshType)
+
+    result = {}
+
+    for seshtype in types:
+        seshs = session.exec(select(Sesh).where(Sesh.type == seshtype)).all()
+        result[seshtype.value] = seshs
+
+    return result
+
+
+@app.get("/quick_stats/", tags=["programming"])
+def get_stats(session: SessionDep):
+    today = date.today()
+    time_total = session.exec(select(func.sum(Sesh.length)).where(Sesh.type == "programming")).one() or 0
+    time_week = session.exec(select(func.sum(Sesh.length)).where(Sesh.type == "programming", Sesh.day >= today - timedelta(days=6))).one() or 0
+    time_fortnight = session.exec(select(func.sum(Sesh.length)).where(Sesh.type == "programming", Sesh.day >= today - timedelta(days=13))).one() or 0
+    time_month = session.exec(select(func.sum(Sesh.length)).where(Sesh.type == "programming", Sesh.day >= today - timedelta(days=29))).one() or 0
+
+    day_streak = 0
+    day_skipped = False
+    check_day = today
+
+    while True:
+        sesh_count = session.exec(select(func.count(Sesh.id)).where(Sesh.day == check_day, Sesh.type == "programming")).one()
+        if sesh_count == 0:
+            break
+        else:
+            check_day -= timedelta(days=1)
+            day_streak += 1
+
+    result = {
+        "time_total_hours": time_total/60,
+        "time_week_hours": time_week/60,
+        "time_fortnight_hours": time_fortnight/60,
+        "time_month_hours": time_month/60,
+
+        "day_streak": day_streak,
+    }
+
+
+    return result
+
+
+
+
+
+
 
